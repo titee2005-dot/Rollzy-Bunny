ea90e8e12e55a25230206841ddbd1d0cd45710c1
Unknown <titee2005@gmail.com>
2026-08-10 19:19:10 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' // 1. เพิ่มบรรทัดนี้
+import { Analytics } from "@vercel/analytics/next"
 
 export default defineConfig({
   plugins: [
